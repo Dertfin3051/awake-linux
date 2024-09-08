d00ecba90714e75ebe557ab8666a5039d891d6c9
Dertfin3051 <dertfin@gmail.com>
2024-09-08 09:51:54 +0300

    v1.0

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo curl -o awake "https://github.com/Dertfin3051/awake-linux/releases/download/1.0/awake-linux"
+sudo chmod +x awake
+sudo mv awake /usr/local/bin/
+
+echo "Installation was successful!"
+echo "Restart terminal & run 'awake' command to run program"
