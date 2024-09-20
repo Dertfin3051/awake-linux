2420c1b7a205a4b2678106886e5ab65222c0208e
dertfin1 <80884846+Dertfin3051@users.noreply.github.com>
2024-09-20 14:32:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo curl -L -o awake "https://github.com/Dertfin3051/awake-linux/releases/download/1.1/awake-linux"
 sudo chmod +x awake
-sudo mv -r awake /usr/local/bin/
+sudo mv -f awake /usr/local/bin/
 
 echo "Installation was successful!"
 echo "Restart terminal & run 'awake' command to run program"
